54aa79c86366d92ccf2036b9336ad9a8e19569e6
Terry Tosh <tmt103@txstate.edu>
2020-09-23 21:53:34 +0000

    Added RecursiveCopyString()

--- main.cpp
+++ main.cpp
@@ -2,19 +2,27 @@
 
 std::string RecursiveReverseString(std::string str, int i, int j);
 int RecursiveCountConsonants(std::string str, int ctr);
+std::string RecursiveCopyString(std::string str, std::string output);
+void RecursivePrintCominations(int arr[]);
 
 int main() {
   std::string str = RecursiveReverseString("doinks", 0, 5);
-  std::cout << str << std::endl;
+  std::cout << "RecursiveReverseString():" << std::endl << str << std::endl;
 
   str = RecursiveReverseString("Techie Delight", 0, 13);
-  std::cout << str << std::endl;
+  std::cout << str << std::endl << std::endl;
 
   int num = RecursiveCountConsonants("geeksforgeeks portal", 0);
-  std::cout << num << std::endl;
+    std::cout << "RecursiveCountConsonants():" << std::endl << num << std::endl;
 
   num = RecursiveCountConsonants("adc34  ueilkdgaeiof", 0);
-  std::cout << num << std::endl;
+  std::cout << num << std::endl << std::endl;
+
+  std::string str2 = RecursiveCopyString("doinks", "");
+  std::cout << "RecursiveCopyString():" << std::endl << str2 << std::endl;
+
+  std::string str3 = RecursiveCopyString("copy_this_string", "");
+  std::cout << str3 << std::endl << std::endl;
 }
 
 std::string RecursiveReverseString(std::string str, int i, int j){
@@ -36,4 +44,12 @@ int RecursiveCountConsonants(std::string str, int ctr){
     ctr++;
   }
   return RecursiveCountConsonants(str.substr(1), ctr);
+}
+
+std::string RecursiveCopyString(std::string str, std::string output){
+  if(str == ""){
+    return output;
+  }
+  output += str[0];
+  return RecursiveCopyString(str.substr(1), output);
 }
